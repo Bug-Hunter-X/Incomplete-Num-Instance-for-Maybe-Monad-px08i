3b87543918c94714710c625c17941d24d8d2b046
BugHunter-X <askzenai@gmail.com>
2025-02-18 08:48:07 +0100

    Add bugSolution.hs file

---
+++ bugSolution.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+instance (Num a) => Num (Maybe a) where
+  (+) (Just x) (Just y) = Just (x + y)
+  (+) _ _ = Nothing
+  (*) (Just x) (Just y) = Just (x * y)
+  (*) _ _ = Nothing
+  (-) (Just x) (Just y) = Just (x - y)
+  (-) _ _ = Nothing
+  abs (Just x) = Just (abs x)
+  abs Nothing = Nothing
+  signum (Just x) = Just (signum x)
+  signum Nothing = Nothing
+  fromInteger x = Just (fromInteger x)
+  negate (Just x) = Just (negate x)
+  negate Nothing = Nothing
+
+main :: IO ()
+main = do
+  print (Just 5 + Just 3)
+  print (Just 5 * Just 3)
+  print (Just 5 - Just 3)
+  print (negate (Just 5))
+  print (negate Nothing) 
